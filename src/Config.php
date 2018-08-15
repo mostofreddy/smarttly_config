2fb55800350d88d386bcdd1ff6852d0edddf1403
Federico Lozada Mosto <mosto.federico@gmail.com>
2018-08-15 11:36:06 -0300

    feat: first commit

---
+++ src/Config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Config
+ *
+ * PHP version 7.2+
+ *
+ * @category  Smarttly/Config
+ * @package   Smarttly/Config
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2018 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      https://github.com/mostofreddy
+ */
+namespace Smarttly\Config;
+
+/**
+ * Config
+ *
+ * @category  Smarttly/Config
+ * @package   Smarttly/Config
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2018 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      https://github.com/mostofreddy
+ */
+class Config
+{
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Bootstrap
+ *
+ * PHP version 7.2+
+ *
+ * @category  Smarttly/Config
+ * @package   Smarttly/Config
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2018 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      https://github.com/mostofreddy
+ */
+$path = realpath(__DIR__.'/../');
+require $path."/vendor/autoload.php";
