d26ae1e57b3dc09234209b86d8717c5f3e20b4ae
Federico Lozada Mosto <mosto.federico@gmail.com>
2018-08-31 18:40:53 -0300

    docs: test autodeploy documentation

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -10,3 +10,10 @@ git config --global user.name "Travis CI"
 
 # build docs
 jekyll build --source ./docs --destination ./docs/_site
+
+# deploy
+cd ./docs/_site
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
