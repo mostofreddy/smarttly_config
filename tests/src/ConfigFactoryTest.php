271dc6968214c9e21138cd54be3eba17dd026a2b
Federico Lozada Mosto <mosto.federico@gmail.com>
2018-09-12 22:25:28 -0300

    tests: ConfigFactory tests

---
+++ tests/src/ConfigFactoryTest.php
@@ -0,0 +1,291 @@
+<?php
+/**
+ * ConfigFactoryTest
+ *
+ * PHP version 7.2+
+ *
+ * @category  Smarttly/Config
+ * @package   Smarttly/Config
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2018 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      https://github.com/mostofreddy
+ */
+namespace Smarttly\Config\Test;
+
+// PHPUnit
+use PHPUnit\Framework\TestCase;
+// Config
+use Smarttly\Config\ConfigFactory;
+use Smarttly\Config\Config;
+use Smarttly\Config\Test\Reader\ReaderHelperTrait;
+
+/**
+ * ConfigFactoryTest
+ *
+ * @category  Smarttly/Config
+ * @package   Smarttly/Config
+ * @author    Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @copyright 2018 Federico Lozada Mosto <mosto.federico@gmail.com>
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      https://github.com/mostofreddy
+ */
+class ConfigFactoryTest extends TestCase
+{
+    use ReaderHelperTrait;
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testLoad()
+    {
+        $content = <<<TXT
+{
+    "say": "hello"
+}
+TXT;
+        $tmpFilename = $this->generateTmpFile($content, 'json');
+
+        $configFactory = new ConfigFactory();
+        $configFactory->load($tmpFilename);
+
+        $this->unlinkFile($tmpFilename);
+
+        $this->assertAttributeEquals(
+            ['say' => 'hello'],
+            'data',
+            $configFactory->getConfig()
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @expectedException        \Smarttly\Config\Exception\InvalidConfigFile
+     * @expectedExceptionMessage File extension cannot be auto-detected: /tmp/dummyFile1
+     *
+     * @return void
+     */
+    public function testGetExtensionFileFail()
+    {
+        $configFactory = new ConfigFactory();
+
+        $refMethod = new \ReflectionMethod(ConfigFactory::class, 'getExtensionFile');
+        $refMethod->setAccessible(true);
+
+        $refMethod->invokeArgs(
+            $configFactory,
+            ['/tmp/dummyFile1']
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testGetExtensionFile()
+    {
+        $content = <<<TXT
+{
+    "say": "hello"
+}
+TXT;
+        $tmpFilename = $this->generateTmpFile($content, 'json');
+
+        $configFactory = new ConfigFactory();
+
+        $refMethod = new \ReflectionMethod(ConfigFactory::class, 'getExtensionFile');
+        $refMethod->setAccessible(true);
+
+        $ext = $refMethod->invokeArgs(
+            $configFactory,
+            [$tmpFilename]
+        );
+
+        $this->unlinkFile($tmpFilename);
+
+        $this->assertEquals('json', $ext);
+    }
+
+    /**
+     * Test method
+     *
+     * @expectedException        \Smarttly\Config\Exception\InvalidConfigFile
+     * @expectedExceptionMessage Unsupported config file extension: .log
+     *
+     * @return void
+     */
+    public function testGetReaderFail()
+    {
+        $configFactory = new ConfigFactory();
+
+        $refMethod = new \ReflectionMethod(ConfigFactory::class, 'getReader');
+        $refMethod->setAccessible(true);
+
+        $refMethod->invokeArgs(
+            $configFactory,
+            ['log']
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testGetReaderJson()
+    {
+        $configFactory = new ConfigFactory();
+
+        $refMethod = new \ReflectionMethod(ConfigFactory::class, 'getReader');
+        $refMethod->setAccessible(true);
+
+        $reader = $refMethod->invokeArgs(
+            $configFactory,
+            ['json']
+        );
+
+        $this->assertInstanceOf(
+            '\Smarttly\Config\Reader\Json',
+            $reader
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testGetReaderPhp()
+    {
+        $configFactory = new ConfigFactory();
+
+        $refMethod = new \ReflectionMethod(ConfigFactory::class, 'getReader');
+        $refMethod->setAccessible(true);
+
+        $reader = $refMethod->invokeArgs(
+            $configFactory,
+            ['php']
+        );
+
+        $this->assertInstanceOf(
+            '\Smarttly\Config\Reader\Php',
+            $reader
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testCreateV1()
+    {
+        $contentJson = <<<TXT
+{
+    "say": "hello",
+    "lastName": "Doe"
+}
+TXT;
+
+        $contentPhp = <<<TXT
+<?php
+
+\$config = [
+    'lastName' => 'Smith',
+    'name' => 'John'
+];
+return \$config;
+TXT;
+
+        $files = [
+            $this->generateTmpFile($contentJson, 'json'),
+            $this->generateTmpFile($contentPhp, 'php')
+        ];
+
+        $config = (ConfigFactory::create(...$files))
+            ->getConfig();
+
+        $this->unlinkFile($files[0]);
+        $this->unlinkFile($files[1]);
+
+        $this->assertAttributeEquals(
+            [
+                'say' => 'hello',
+                'lastName' => 'Smith',
+                'name' => 'John'
+            ],
+            'data',
+            $config
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testCreateV2()
+    {
+        $contentPhp1 = <<<TXT
+<?php
+
+\$config = [
+    'say' => 'hello',
+    'lastName' => 'Doe'
+];
+return \$config;
+TXT;
+
+        $contentPhp2 = <<<TXT
+<?php
+
+\$config = [
+    'lastName' => 'Smith',
+    'name' => 'John'
+];
+return \$config;
+TXT;
+
+        $files = [
+            $this->generateTmpFile($contentPhp1, 'php'),
+            $this->generateTmpFile($contentPhp2, 'php')
+        ];
+
+        $config = (ConfigFactory::create(...$files))
+            ->getConfig();
+
+        $this->unlinkFile($files[0]);
+        $this->unlinkFile($files[1]);
+
+        $this->assertAttributeEquals(
+            [
+                'say' => 'hello',
+                'lastName' => 'Smith',
+                'name' => 'John'
+            ],
+            'data',
+            $config
+        );
+    }
+
+    /**
+     * Test method
+     *
+     * @return void
+     */
+    public function testCreateEmpty()
+    {
+        $config = (ConfigFactory::create())
+            ->getConfig();
+
+        $this->assertAttributeEquals(
+            [],
+            'data',
+            $config
+        );
+    }
+}
--- tests/src/Reader/ReaderHelperTrait.php
+++ tests/src/Reader/ReaderHelperTrait.php
@@ -29,12 +29,18 @@ trait ReaderHelperTrait
      * Generate tmp file
      *
      * @param string $content Content file
+     * @param string $ext     Extension file. Default: null
      *
      * @return string
      */
-    protected function generateTmpFile(string $content):string
+    protected function generateTmpFile(string $content, string $ext = null):string
     {
         $tmpFilename = tempnam(sys_get_temp_dir(), "config");
+
+        if (!is_null($ext)) {
+            $tmpFilename .= '.'.$ext;
+        }
+
         file_put_contents($tmpFilename, $content);
         return $tmpFilename;
     }
